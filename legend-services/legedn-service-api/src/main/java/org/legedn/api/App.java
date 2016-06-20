5ff769fc2bcc22cca66e75a79ca85690748992a1
lipeng <15260435650@163.com>
2016-06-20 13:44:54 +0800

    项目初始化

---
+++ legend-services/legedn-service-api/src/main/java/org/legedn/api/App.java
@@ -0,0 +1,13 @@
+package org.legedn.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
